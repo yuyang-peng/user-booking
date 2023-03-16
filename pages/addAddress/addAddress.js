fe780b4b0fb1da308bac6de8882ee8b450bccf56
kongdingli <1242062194@qq.com>
2023-03-16 21:37:19 +0800

    删除多余文件

--- pages/addAddress/addAddress.js
+++ pages/addAddress/addAddress.js
@@ -5,88 +5,62 @@ Page({
    * 页面的初始数据
    */
   data: {
-    region: ['', '', ''],
+
   },
-  // 
-  bindRegionChange: function (e) {
-    // console.log('picker发送选择改变，携带值为', e.detail.value)
-    this.setData({
-      region: e.detail.value
-    })
-  },
-  // 
-  formSubmit: function (e) {
-    // console.log('form发生了submit事件，携带数据为：', e.detail.value)
-    var str = e.detail.value;
-    
-    if (str){
-      wx.reLaunch({
-        url: '/pages/user/user',
-        success: function (){
-          wx.showToast({
-            title: '创建成功！',
-          })
-          wx.setStorageSync("addInfo", str)
-        }
-      })
-    }
-  },
-  formReset:function(){
-    console.log("form发生reset事件")
-  },
+
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage() {
 
   }
 })
